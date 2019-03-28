446c9cf6d3c59db5b3cdf2387abc25c3a0476472
derricklewis <derrickjameslewis@gmail.com>
2019-03-28 16:57:02 -0400

    text clean

--- citibike_scripts_derrick.py
+++ citibike_scripts_derrick.py
@@ -1,6 +1,14 @@
 
 # Functions to be added to the main script. 
-
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, LabelEncoder
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.model_selection import GridSearchCV
+import itertools
 
 
 def show_cf(y_true, y_pred, class_names=None, model_name=None):
@@ -35,3 +43,23 @@ def plot_feature_importances(model):
     plt.yticks(np.arange(n_features), X_train.columns.values) 
     plt.xlabel("Feature importance")
     plt.ylabel("Feature")
+    
+def svc_param_selection(X, y, nfolds, kern=str):
+    Cs = [0.001, 0.01, 0.1, 1, 10]
+    gammas = [0.001, 0.01, 0.1, 1]
+    param_grid = {'C': Cs, 'gamma' : gammas}
+    grid_search = GridSearchCV(svm.SVC(kernel=kern), param_grid, cv=nfolds)
+    grid_search.fit(X, y)
+    grid_search.best_params_
+    return grid_search.best_params_
+
+def svm_function(X, y, Xt, yt, kern, gamma1, rand, c1=1, **kwargs):
+    clf = svm.SVC(kernel=kern, random_state=rand, C=c1, **kwargs)
+    clf.fit(X, y)
+    training_preds = clf.predict(X)
+    val_preds = clf.predict(Xt)
+    training_accuracy = accuracy_score(y, training_preds)
+    val_accuracy = accuracy_score(yt, val_preds)
+    print("Training Accuracy: {:.4}%".format(training_accuracy * 100))
+    print("Validation accuracy: {:.4}%".format(val_accuracy * 100))
+    return show_cf(yt, val_preds)
